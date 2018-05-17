67f58fde24ed60f51c0c9f37c4bcc8060b950db5
indratamagiri <madegiri1212@gmail.com>
2018-05-17 15:34:24 +0700

    Update data staff

--- database/SQLQuery1.sql
+++ database/SQLQuery1.sql
@@ -73,7 +73,7 @@ drop table customer
  
 Insert into staff values('STF001', 'Giri', 'Jalan U', '1997/02/21', '0877328132', 'made@mail.com', 2000000, 'Desser')
 Insert into staff values('STF002', 'Patrick', 'Jalan Samping', '1992/11/01', '0877234158', 'patrick@mail.com', 2300000, 'Main Curse')
-Insert into staff values('STF003', 'Tommy', 'Jalan Sandung', '1990/12/21', '0877612456', 'tommy@mail.com', 3000000, 'Finnacial Officer')
+Insert into staff values('STF003', 'Tommy', 'Jalan Sandung', '1990/12/21', '0877612456', 'tommy@mail.com', 3600000, 'Finnacial Officer')
 Insert into staff values('STF004', 'Andrew', 'Jalan Selatan', '1997/01/01', '0877545236', 'andrew@mail.com', 2500000, 'Beverages')
 Insert into staff values('STF005', 'Topan', 'Jalan Angin', '1995/05/08', '0877541523', 'topan@mail.com', 2000000, 'Desser')
 Insert into staff values('STF006', 'Ekho', 'Jalan Raya', '1992/07/11', '08773124523', 'ekho@mail.com', 3600000, 'Financial Officer')
@@ -81,9 +81,11 @@ Insert into staff values('STF007', 'Brily', 'Jalan Utara', '1998/01/30', '087784
 Insert into staff values('STF008', 'Chris', 'Jalan Temang', '1995/09/04', '08774950434', 'chris@mail.com', 2000000, 'Desser')
 Insert into staff values('STF009', 'Bram', 'Jalan Selatan', '1993/12/30', '08773421456', 'bram@mail.com', 2800000, 'Branch Appetizer')
 Insert into staff values('STF010', 'Samuel', 'Jalan Kecil', '1994/05/28', '08773571356', 'samuel@mail.com', 2800000, 'Branch Appetizer')
-Insert into staff values('STF011', '', 'Jalan U', '1997/02/21', '0877328132', 'made@mail.com', 2000000, 'Desser')
-Insert into staff values('STF012', 'Giri', 'Jalan U', '1997/02/21', '0877328132', 'made@mail.com', 2000000, 'Desser')
-Insert into staff values('STF013', 'Giri', 'Jalan U', '1997/02/21', '0877328132', 'made@mail.com', 2000000, 'Desser')
-Insert into staff values('STF014', 'Giri', 'Jalan U', '1997/02/21', '0877328132', 'made@mail.com', 2000000, 'Desser')
-Insert into staff values('STF015', 'Giri', 'Jalan U', '1997/02/21', '0877328132', 'made@mail.com', 2000000, 'Desser')
-select * from staff
+Insert into staff values('STF011', 'Yasir', 'Jalan Selempang', '1996/08/11', '08773451234', 'yasir@mail.com', 2000000, 'Desser')
+Insert into staff values('STF012', 'Alvin', 'Jalan Rakyat', '1989/07/09', '08776331634', 'alvin@mail.com', 3600000, 'Finnacial Officer')
+Insert into staff values('STF013', 'Tamara', 'Jalan Shydan', '1998/12/03', '08773425463', 'tamara@mail.com', 2500000, 'Beverages')
+Insert into staff values('STF014', 'Kaleb', 'Jalan Keluarga', '1992/07/29', '08773451235', 'kaleb@mail.com', 2500000, 'Beverages')
+Insert into staff values('STF015', 'Bianka', 'Jalan Salak', '1993/06/09', '08770948239', 'bianka@mail.com', 2300000, 'Main Curse')
+
+
+select * from staff 
