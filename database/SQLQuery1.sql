e7f90d4b0d3741498efd95d7d48d59a08f560dfe
indratamagiri <madegiri1212@gmail.com>
2018-05-19 07:04:16 +0700

    input vendor

--- database/SQLQuery1.sql
+++ database/SQLQuery1.sql
@@ -14,7 +14,7 @@ Create table Vendor (
 	VendorID char(9) primary key not null check(VendorID like 'VEN[0-9][0-9][0-9]'),
 	VendorName varchar(30) not null,
 	VendorAddress varchar(30) not null,
-	VendorEmail varchar(30) not null check(VendorEmail like '%@%' and VendorEmail like '%.co.id'),
+	VendorEmail varchar(30) not null check(VendorEmail like '%@%' and VendorEmail like '%.com'),
 	VendorPhoneNumber varchar(30) not null
 )
 
@@ -47,6 +47,7 @@ Create table Product(
 
 Create table Customer(
 	CustomerID char(9) primary key not null check(CustomerId like 'CUS[0-9][0-9][0-9]'),
+	CustomerName varchar(30) not null,
 	CustomerAddress varchar(30) not null,
 	CustomerDOB date not null,
 	CustomerPhone varchar(30) check(LEN(CustomerPhone) > 10),
@@ -71,6 +72,7 @@ drop table product
 drop table salesTransaction
 drop table customer
  
+ --Staff
 Insert into Staff values('STF001', 'Giri', 'Jalan U', '1997/02/21', '0877328132', 'made@mail.com', 2000000, 'Desser')
 Insert into Staff values('STF002', 'Patrick', 'Jalan Samping', '1992/11/01', '0877234158', 'patrick@mail.com', 2300000, 'Main Curse')
 Insert into Staff values('STF003', 'Tommy', 'Jalan Sandung', '1990/12/21', '0877612456', 'tommy@mail.com', 3600000, 'Finnacial Officer')
@@ -87,7 +89,26 @@ Insert into Staff values('STF013', 'Tamara', 'Jalan Shydan', '1998/12/03', '0877
 Insert into Staff values('STF014', 'Kaleb', 'Jalan Keluarga', '1992/07/29', '08773451235', 'kaleb@mail.com', 2500000, 'Beverages')
 Insert into Staff values('STF015', 'Bianka', 'Jalan Salak', '1993/06/09', '08770948239', 'bianka@mail.com', 2300000, 'Main Curse')
 
-Insert into Vendor values('VEN001', 
+
+--Vendor
+Insert into Vendor values('VEN001', 'Bintang21', 'Jalan Menteng Raya no 33', 'bintang21@mail.com', '08173849383')
+Insert into Vendor values('VEN002', 'Diantara', 'JL. Batas Dukusari No 5', 'diantara@mail.com', '08173829345')
+Insert into Vendor values('VEN003', 'Mandailing', 'Ruko Permai No 3 Block E', 'mandailing@mail.com', '08183720912')
+Insert into Vendor values('VEN004', 'PT Gitaswara Indonesia', 'Jalan Jend Sudirman no 9 ', 'gitaswaraIndo@mail.com', '0814418537')
+Insert into Vendor values('VEN005', 'PT Gubah Bumi Selaras', 'Jl Jend Sudirman Kompl no 24C', 'bumiGubah@mail.com', '08177645623')
+Insert into Vendor values('VEN006', 'PT Pangansari Utama Food', 'Jl Jend Sudirman 880', 'Utamafood@mail.com', '0817761551')
+Insert into Vendor values('VEN007', 'PT Kiat Sukses', 'Jl Syarifudin Yose 5', 'suksesKia@mail.com', '0817876723')
+Insert into Vendor values('VEN008', 'PT Resa Kalimantan Sakti', 'Jl Letjen Suprapto 1', 'Sakti004@mail.com', '0817424386')
+Insert into Vendor values('VEN009', 'PT Tata Adhis Thana', 'Jl Mulawarman 12', 'tata_adhis@mail.com', '0817743138')
+Insert into Vendor values('VEN010', 'Aneka Makmur', 'Jl Peta Lingkar no 41', 'aneka_makmur49@mail.com', '08176035001')
+Insert into Vendor values('VEN011', 'PT Arta Boga Cemerlang', 'Jl Soekarno Hatta 70', 'cermelang33@mail.com', '08177300791')
+Insert into Vendor values('VEN012', 'PD Betani', 'Jl Kopo 104', 'betani@mail.com', '08176010173')
+Insert into Vendor values('VEN013', 'Cahaya Mulya Mandiri', 'Jl Papanggungan 36', 'mulyaMandiri@mail.com', '08177300203')
+Insert into Vendor values('VEN014', 'CV Cemerlang Abadi', 'Jl Zamrud 11', 'Abadijaya@mail.com', '08177307635')
+Insert into Vendor values('VEN015', 'PT Di-Marta', 'Jl Rebana 2', 'Martadinata@mail.com', '08177305121')
+
+
 
 
 select * from Staff 
+select * from Vendor
